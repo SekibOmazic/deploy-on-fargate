be4b4f984ed4c91ce333b212ef799ce2bab1ed0f
Sekib Omazic <sekib.omazic-ext@rio.cloud>
2021-04-05 15:41:19 +0200

    add missing dependencies

--- infra/rolling/lib/service-stack.ts
+++ infra/rolling/lib/service-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from '@aws-cdk/core'
 
-import { Certificate } from '@aws-cdk/aws-certificatemanager'
-import { Alarm } from '@aws-cdk/aws-cloudwatch'
-import { Repository } from '@aws-cdk/aws-ecr'
-import { Cluster, ContainerImage, PropagatedTagSource } from '@aws-cdk/aws-ecs'
-import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns'
-import { HttpCodeTarget } from '@aws-cdk/aws-elasticloadbalancingv2'
-import { HostedZone } from '@aws-cdk/aws-route53'
-import { StringParameter } from '@aws-cdk/aws-ssm'
+import * as acm from '@aws-cdk/aws-certificatemanager'
+import * as cw from '@aws-cdk/aws-cloudwatch'
+import * as ecr from '@aws-cdk/aws-ecr'
+import * as ecs from '@aws-cdk/aws-ecs'
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns'
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2'
+import * as route53 from '@aws-cdk/aws-route53'
+import * as ssm from '@aws-cdk/aws-ssm'
 
 export interface ServiceStackProps extends cdk.StackProps {
   readonly domainName: string
@@ -16,17 +16,17 @@ export interface ServiceStackProps extends cdk.StackProps {
 }
 
 export class ServiceStack extends cdk.Stack {
-  private fargateService: ApplicationLoadBalancedFargateService
+  private fargateService: ecs_patterns.ApplicationLoadBalancedFargateService
 
   constructor(scope: cdk.Construct, id: string, props: ServiceStackProps) {
     super(scope, id, props)
 
-    const domainZone = HostedZone.fromLookup(this, 'Zone', {
+    const domainZone = route53.HostedZone.fromLookup(this, 'Zone', {
       domainName: props.domainZone,
     })
 
     // Lookup pre-existing TLS certificate
-    const certificateArn = StringParameter.fromStringParameterAttributes(
+    const certificateArn = ssm.StringParameter.fromStringParameterAttributes(
       this,
       'CertArnParameter',
       {
@@ -34,28 +34,28 @@ export class ServiceStack extends cdk.Stack {
       }
     ).stringValue
 
-    const certificate = Certificate.fromCertificateArn(
+    const certificate = acm.Certificate.fromCertificateArn(
       this,
       'Cert',
       certificateArn
     )
 
     // ECR repository for the docker images
-    const ecrRepo = Repository.fromRepositoryName(
+    const ecrRepo = ecr.Repository.fromRepositoryName(
       this,
       'Repo',
       props.ecrRepoName
     )
     const tag = process.env.IMAGE_TAG ? process.env.IMAGE_TAG : 'latest'
-    const image = ContainerImage.fromEcrRepository(ecrRepo, tag)
+    const image = ecs.ContainerImage.fromEcrRepository(ecrRepo, tag)
 
     // ECS
-    const cluster = new Cluster(this, 'Cluster', {
+    const cluster = new ecs.Cluster(this, 'Cluster', {
       clusterName: props.domainName.replace(/\./g, '-'),
       containerInsights: true,
     })
 
-    this.fargateService = new ApplicationLoadBalancedFargateService(
+    this.fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
       this,
       'Service',
       {
@@ -65,22 +65,22 @@ export class ServiceStack extends cdk.Stack {
         domainName: props.domainName,
         domainZone,
         certificate,
-        propagateTags: PropagatedTagSource.SERVICE,
+        propagateTags: ecs.PropagatedTagSource.SERVICE,
       }
     )
 
     // Alarms: monitor 500s and unhealthy hosts on target groups
-    new Alarm(this, 'TargetGroupUnhealthyHosts', {
+    new cw.Alarm(this, 'TargetGroupUnhealthyHosts', {
       alarmName: props.ecrRepoName + '-Unhealthy-Hosts',
       metric: this.fargateService.targetGroup.metricUnhealthyHostCount(),
       threshold: 1,
       evaluationPeriods: 2,
     })
 
-    new Alarm(this, 'TargetGroup5xx', {
+    new cw.Alarm(this, 'TargetGroup5xx', {
       alarmName: props.ecrRepoName + '-Http-500',
       metric: this.fargateService.targetGroup.metricHttpCodeTarget(
-        HttpCodeTarget.TARGET_5XX_COUNT
+        elb.HttpCodeTarget.TARGET_5XX_COUNT
       ),
       threshold: 1,
       evaluationPeriods: 1,
