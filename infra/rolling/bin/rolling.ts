a8592befedf5a51b5331f51f10c22ca0fec467d8
Sekib Omazic <sekib.omazic-ext@rio.cloud>
2021-04-05 18:00:31 +0200

    fix ecr repo name

--- infra/rolling/bin/rolling.ts
+++ infra/rolling/bin/rolling.ts
@@ -8,7 +8,6 @@ const GITHUB_REPO_NAME = 'deploy-on-fargate'
 const GITHUB_REPO_OWNER = 'SekibOmazic'
 const DOMAIN_NAME = 'simple-api.delta-comsysto-reply.de'
 const DOMAIN_ZONE = 'delta-comsysto-reply.de'
-const ECR_REPOSITORY_NAME = 'simple-api'
 
 const app = new cdk.App()
 
@@ -16,7 +15,7 @@ new ServiceStack(app, 'SimpleApi', {
   stackName: 'SimpleApi',
   domainName: DOMAIN_NAME,
   domainZone: DOMAIN_ZONE,
-  ecrRepoName: ECR_REPOSITORY_NAME,
+  ecrRepoName: GITHUB_REPO_NAME, // just a convention I like: github repo and ECR have the same name
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
